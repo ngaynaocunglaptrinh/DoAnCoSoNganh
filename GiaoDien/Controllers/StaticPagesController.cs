7b6660750476b5936080bb037425a97c21b2f3d0
ngaynaocunglaptrinh <quochoccode17082004@gmail.com>
2024-09-28 09:23:12 +0700

    add

---
+++ GiaoDien/Controllers/StaticPagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiaoDien.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        // GET: StaticPages
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
